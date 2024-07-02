4b9b93699537806e99db8c69145e62f64842e5e9
Silas <silasmelo12@gmail.com>
2024-07-02 20:59:04 -0300

    Criação do ProductService e implementação do método de listAll().

---
+++ src/main/java/br/com/maximusDesenvolvimentoHQ/MiniEcommerce/service/ProductService.java
@@ -0,0 +1,14 @@
+package br.com.maximusDesenvolvimentoHQ.MiniEcommerce.service;
+
+import br.com.maximusDesenvolvimentoHQ.MiniEcommerce.domain.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    public List<Product> listAll(){
+        return List.of(new Product(2L,"iphone",5000,"tecnologia"));
+    }
+}
