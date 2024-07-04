fa05fbc705dd941dada09abbe4bc6e1035fef6cd
Silas Melo <silasmelo12@gmail.com>
2024-07-04 17:45:05 -0300

    Handler para validação de campos. Padronização das exceções para validação do campo.

--- src/main/java/br/com/maximusDesenvolvimentoHQ/MiniEcommerce/handler/RestExceptionHandler.java
+++ src/main/java/br/com/maximusDesenvolvimentoHQ/MiniEcommerce/handler/RestExceptionHandler.java
@@ -2,18 +2,25 @@ package br.com.maximusDesenvolvimentoHQ.MiniEcommerce.handler;
 
 import br.com.maximusDesenvolvimentoHQ.MiniEcommerce.exception.BadRequestException;
 import br.com.maximusDesenvolvimentoHQ.MiniEcommerce.exception.BadRequestExceptionDetails;
+import br.com.maximusDesenvolvimentoHQ.MiniEcommerce.exception.ValidationExceptionDetails;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
+@Log4j2
 public class RestExceptionHandler {
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<BadRequestExceptionDetails> badRequestExceptionDetailsResponseEntity(BadRequestException badRequestException){
+    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestExceptionDetailsResponseEntity(BadRequestException badRequestException){
         return new ResponseEntity<>(
                 BadRequestExceptionDetails.builder()
                         .timestamp(LocalDateTime.now())
@@ -23,4 +30,28 @@ public class RestExceptionHandler {
                         .developerMessage(badRequestException.getClass().getName())
                         .build(),HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception){
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
+        String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
+
+        log.info("Fields {}",exception.getBindingResult().getFieldError().getField());
+        return new ResponseEntity<>(
+                ValidationExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception, campos inválidos.")
+                        .details("Check os campos de erros")
+                        .developerMessage(exception.getClass().getName())
+                        .fields(fields)
+                        .fieldsMessage(fieldsMessage)
+                        .build(),HttpStatus.BAD_REQUEST);
+    }
+
+
+
+
 }
